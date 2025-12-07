fb5b0faeb4b3ea38172d289d64135e2be19a3cfa
Morgan Hertel <hertelmorgan2@gmail.com>
2025-12-07 15:49:56 -0700

    Added ggplot2 library - MH

--- Dog_Breed.R
+++ Dog_Breed.R
@@ -7,5 +7,3 @@
 breed_traits <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2022/2022-02-01/breed_traits.csv')
 trait_description <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2022/2022-02-01/trait_description.csv')
 breed_rank_all <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2022/2022-02-01/breed_rank.csv')
-
-#
